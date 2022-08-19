1615eee75904e2e336beca3974e67096fa1f0fe4
vajmera <f20170693@pilani.bits-pilani.ac.in>
2022-08-19 11:33:57 +0530

    implemented recycler view with static data

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -1,4 +1,30 @@
 package com.example.hellokotlin
 
-class MainActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity: AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager?=null;
+    private var adapter:RecyclerView.Adapter<MyAdapter.ViewHolder>?=null; //if null then assign null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+        layoutManager=LinearLayoutManager(this);
+
+        //recyclerView.layoutManager=layoutManager;
+         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
+        recyclerView.layoutManager = layoutManager
+
+
+        adapter=MyAdapter();
+        recyclerView.adapter=adapter;
+
+    }
 }
